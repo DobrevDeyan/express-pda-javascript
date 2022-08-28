fd96f853cf3bbf4c080ac14b516d98fde6f31b01
deyandobrev <dobrev.de@gmail.com>
2022-08-28 23:35:46 +0300

    homePageStyling

--- src/views/home-page.js
+++ src/views/home-page.js
@@ -2,10 +2,11 @@ import { html } from "../utilities/lib.js"
 
 const homeTemplate = () => html` <section id="home">
   <!-- <img src="../../images/Container ship.gif" alt="shipping gif" /> -->
-  <h1>Welcome to Express PDA</h1>
+  <h1>Express PDA</h1>
+  <h2>PDAs made easy</h2>
   <h2>
-    Use a powerful web tool to get instant access to the financial information
-    for your port of choosing and plan your vessel fixture
+    Reliable tool to get instant access to financial information for your
+    terminal of fixture.
   </h2>
 </section>`
 export function homePage(ctx) {

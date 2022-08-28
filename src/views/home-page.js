208138a9561a13213a73a59a0f873da783b1047d
deyandobrev <dobrev.de@gmail.com>
2022-08-29 01:05:07 +0300

    homePageStyling

--- src/views/home-page.js
+++ src/views/home-page.js
@@ -2,12 +2,9 @@ import { html } from "../utilities/lib.js"
 
 const homeTemplate = () => html` <section id="home">
   <!-- <img src="../../images/Container ship.gif" alt="shipping gif" /> -->
-  <h1>Express PDA</h1>
+  <h1>Express <span>PDA</span></h1>
   <h2>PDAs made easy</h2>
-  <h2>
-    Reliable tool to get instant access to financial information for your
-    terminal of fixture.
-  </h2>
+  <h2>Voyage estimations for your future fixtures</h2>
 </section>`
 export function homePage(ctx) {
   ctx.render(homeTemplate())

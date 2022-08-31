312f1e3fc29d247aa26df63962554e8f382a2be9
deyandobrev <dobrev.de@gmail.com>
2022-08-31 22:30:38 +0300

    navIndicatorWorking

--- src/views/home-page.js
+++ src/views/home-page.js
@@ -1,11 +1,26 @@
 import { html } from "../utilities/lib.js"
 
 const homeTemplate = () => html` <section id="home">
-  <!-- <img src="../../images/Container ship.gif" alt="shipping gif" /> -->
   <h1>Express <span>PDA</span></h1>
   <h2>PDAs made easy</h2>
-  <h2>Voyage estimates for your future fixtures</h2>
+  <h2>
+    <span class="typing"></span>
+  </h2>
 </section>`
 export function homePage(ctx) {
   ctx.render(homeTemplate())
+
+  // TYPING ANIMATION
+  let typed = new Typed(".typing", {
+    strings: [
+      "",
+      "Voyage estimates for your future fixtures",
+      "24 hour access on every device",
+      "Port information and services available",
+    ],
+    typeSpeed: 60,
+    BackSpeed: 60,
+    loop: true,
+    // cursorChar: "|",
+  })
 }

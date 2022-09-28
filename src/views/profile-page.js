b92f83c008cde2411d5f853a1b29b85496bd7504
deyandobrev <dobrev.de@gmail.com>
2022-09-28 23:50:18 +0300

    userProfile

--- src/views/profile-page.js
+++ src/views/profile-page.js
@@ -16,10 +16,20 @@ const profileTemplate = () => html` <section id="profile">
         alt="profile-picture"
         class="profile-picture"
       />
-      <p class="profile-info">
-        Welcome to your user page. Here you can find useful information about
-        your stay and usage of the platform.
-      </p>
+      <div class="misc-info">
+        <h1>My profile</h1>
+        <div class="misc-info-details">
+          <p>Last login: 11.11.1111 04:54</p>
+          <p>Adress Rozq 25 vh B etaj 2</p>
+        </div>
+      </div>
+      <div class="user-info">
+        <p>Name</p>
+        <p>Phone:</p>
+        <p>Email</p>
+        <p>SMS Alert: On</p>
+      </div>
+      <button type="button" class="generate-btn">Generate PDAs</button>
     </div>
     <div class="payments-container">
       <p class="profile-info">

0e5d75eb459536e5553678775e783e38f308d696
deyandobrev <dobrev.de@gmail.com>
2022-09-29 13:46:47 +0300

    userProfile

--- src/views/profile-page.js
+++ src/views/profile-page.js
@@ -1,12 +1,13 @@
 import { html } from "../utilities/lib.js"
 import { getUserData } from "../utilities/util.js"
 
-const profileTemplate = () => html` <section id="profile">
+const profileTemplate = (user) => html` <section id="profile">
   <div class="profile-container">
     <h1 class="profile-title">User profile</h1>
     <p class="profile-info">
       Welcome to your user page. Here you can find useful information about your
-      stay and usage of the platform.
+      stay and usage of the platform. In addition you can add a user name and
+      phone number in the form below.
     </p>
   </div>
   <div class="profile-wrapper">
@@ -24,12 +25,27 @@ const profileTemplate = () => html` <section id="profile">
         </div>
       </div>
       <div class="user-info">
-        <p>Name</p>
-        <p>Phone:</p>
-        <p>Email</p>
-        <p>SMS Alert: On</p>
+        <div class="misc-user-details">
+          <form class="form-update-user-info">
+            <input type="text" name="name" value="${user.id}" placeholder="" />
+            <input
+              type="text"
+              name="name"
+              placeholder="+359 888 888 888"
+              value="${user.phone}"
+            />
+          </form>
+        </div>
+        <p class="mail">Registered email: ${user.email}</p>
+        <div class="sms">
+          <p>SMS alert activation:</p>
+          <label class="switch">
+            <input type="checkbox" />
+            <span class="slider round"></span>
+          </label>
+        </div>
       </div>
-      <button type="button" class="generate-btn">Generate PDAs</button>
+      <button type="button" class="save-btn">Save</button>
     </div>
     <div class="payments-container">
       <p class="profile-info">
@@ -47,5 +63,6 @@ const profileTemplate = () => html` <section id="profile">
 </section>`
 
 export async function profilePage(ctx) {
-  ctx.render(profileTemplate())
+  const user = getUserData()
+  ctx.render(profileTemplate(user))
 }

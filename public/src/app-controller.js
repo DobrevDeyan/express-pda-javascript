d17f6e1b1b7eb0f465d934da79cec23aa7352a27
deyandobrev <dobrev.de@gmail.com>
2022-10-16 14:12:53 +0300

    styling

--- public/src/app-controller.js
+++ public/src/app-controller.js
@@ -1,4 +1,4 @@
-import { page, render, navigationSlider } from "./utilities/lib.js"
+import { page, render } from "./utilities/lib.js"
 import { onAuthStateChanged, auth } from "./firebase/firebase-setup.js"
 import { setUserData, getUserData, clearUserData } from "./utilities/util.js"
 import { homePage } from "./views/home-page.js"
@@ -8,27 +8,18 @@ import { registerPage } from "./views/register-page.js"
 import { dashboardPage } from "./views/user-dashboard-page.js"
 import { createPdaPage } from "./views/create-pda-page.js"
 import { profilePage } from "./views/profile-page.js"
-// import { logout } from "./api/api.js"
-// import { detailsPage } from "./views/details.js"
-// import { editPage } from "./views/edit.js"
-// import { searchPage } from "./views/search.js"
 
 const root = document.getElementById("site-content")
 document.getElementById("logoutButton").addEventListener("click", onLogout)
 
 page(decorateContext)
 page("/", homePage)
-// page("/", createPdaPage)
-// page("/", profilePage)
-// page("/", dashboardPage)
 page("/faq", faqPage)
 page("/login", loginPage)
 page("/register", registerPage)
 page("/dashboard", dashboardPage)
 page("/create-pda", createPdaPage)
 page("/profile", profilePage)
-// page("/details/:id", detailsPage)
-// page("/edit/:id", editPage)
 
 // Initiate app
 updateUserNav()
@@ -53,14 +44,8 @@ function updateUserNav() {
   if (userData) {
     document.querySelector(".user").style.display = "inline-block"
     document.querySelector(".guest").style.display = "none"
-    // document.querySelector(".user span").textContent = userData.email
-    // document.querySelector(".user span").style.color = "alice"
-    // const target = document.querySelector("nav .user .marker")
-    // navigationSlider(target)
   } else {
     document.querySelector(".user").style.display = "none"
     document.querySelector(".guest").style.display = "inline-block"
-    // const target = document.querySelector("nav .guest .marker")
-    // navigationSlider(target)
   }
 }
--- public/src/utilities/lib.js
+++ public/src/utilities/lib.js
@@ -1,27 +1,6 @@
-import { html, render } from "../../node_modules/lit-html/lit-html.js"
-import page from "../../node_modules/page/page.mjs"
+// import { html, render } from "../../node_modules/lit-html/lit-html.js"
+// import page from "../../node_modules/page/page.mjs"
+import { html, render } from "../../assets/lit-html/lit-html.js"
+import page from "../../assets/page/page.mjs"
 
-function navigationSlider(target) {
-  const items = document.querySelectorAll("header nav a")
-
-  // console.log(items)
-  // const additionalLinks = document.querySelectorAll(".nav-link")
-  // document.addEventListener("click", (event) => {
-  //   if (event.target.classList.contains("nav-link")) {
-  //     console.log(event.target)
-  //   }
-  // })
-  // console.log(additionalLinks)
-
-  function indicator(e) {
-    target.style.left = e.offsetLeft + "px"
-    target.style.width = e.offsetWidth + "px"
-  }
-  items.forEach((link) => {
-    link.addEventListener("click", (e) => {
-      indicator(e.target)
-    })
-  })
-}
-
-export { html, render, page, navigationSlider }
+export { html, render, page }
